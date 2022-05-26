4cf990b5465c1584afc5eb606e9169f8b23fe861
JRamos29 <malebolge@gmail.com>
2022-05-26 18:06:01 -0300

    Simple query with @Query

--- src/main/java/com/jrammos29/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/jrammos29/workshopmongo/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.jrammos29.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.jrammos29.workshopmongo.domain.Post;
@@ -12,8 +13,8 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
 
-//	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
-//	List<Post> searchTitle(String text);
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 
 //	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
 //	List<Post> fullSearch(String text, Date minDate, Date maxDate);
