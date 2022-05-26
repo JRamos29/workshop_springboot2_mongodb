dbb2faa8e5695a23f4ff7ff996135890aae1cc00
JRamos29 <malebolge@gmail.com>
2022-05-26 17:58:02 -0300

    SpringBoot Query Methods

--- src/main/java/com/jrammos29/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/jrammos29/workshopmongo/repositories/PostRepository.java
@@ -1,5 +1,7 @@
 package com.jrammos29.workshopmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,13 @@ import com.jrammos29.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+	List<Post> findByTitleContainingIgnoreCase(String text);
+
+//	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+//	List<Post> searchTitle(String text);
+
+//	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+//	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 
 }
--- src/main/java/com/jrammos29/workshopmongo/resources/PostResource.java
+++ src/main/java/com/jrammos29/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.jrammos29.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jrammos29.workshopmongo.domain.Post;
+import com.jrammos29.workshopmongo.resources.utils.URL;
 import com.jrammos29.workshopmongo.services.PostService;
 
 @RestController
@@ -23,13 +27,13 @@ public class PostResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
-//	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
-// 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
-//		text = URL.decodeParam(text);
-//		List<Post> list = service.findByTitle(text);
-//		return ResponseEntity.ok().body(list);
-//	}
-//
+	@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+
 //	@RequestMapping(value="/fullsearch", method=RequestMethod.GET)
 // 	public ResponseEntity<List<Post>> fullSearch(
 // 			@RequestParam(value="text", defaultValue="") String text,
