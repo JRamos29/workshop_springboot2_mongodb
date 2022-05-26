e503b6fb66359c0de451b4ed329025b2a76ee5a5
JRamos29 <malebolge@gmail.com>
2022-05-26 14:26:17 -0300

    Project created

---
+++ src/test/java/com/jrammos29/workshopmongo/WorkshopSpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrammos29.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
