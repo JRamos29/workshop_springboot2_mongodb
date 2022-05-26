eba80f11a140bf97b1145a82dcc33aea3366d1a2
JRamos29 <malebolge@gmail.com>
2022-05-26 16:55:16 -0300

    Creating entity Post with nested User

--- src/main/java/com/jrammos29/workshopmongo/config/Initialization.java
+++ src/main/java/com/jrammos29/workshopmongo/config/Initialization.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.jrammos29.workshopmongo.domain.Post;
 //import com.jrammos29.workshopmongo.domain.Post;
 import com.jrammos29.workshopmongo.domain.User;
+import com.jrammos29.workshopmongo.repositories.PostRepository;
 //import com.jrammos29.workshopmongo.dto.AuthorDTO;
 //import com.jrammos29.workshopmongo.dto.CommentDTO;
 import com.jrammos29.workshopmongo.repositories.UserRepository;
@@ -21,23 +23,26 @@ public class Initialization implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 
-//	@Autowired
-//	private PostRepository postReposiroty;
+	@Autowired
+	private PostRepository postReposiroty;
 
 	@Override
 	public void run(String... arg0) throws Exception {
 		
-//		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-//		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		userRepository.deleteAll();
-//		postReposiroty.deleteAll();
+		postReposiroty.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
 
 //		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 //		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
@@ -49,7 +54,7 @@ public class Initialization implements CommandLineRunner {
 //		post1.getComments().addAll(Arrays.asList(c1, c2));
 //		post2.getComments().addAll(Arrays.asList(c3));
 		
-//		postReposiroty.saveAll(Arrays.asList(post1, post2));
+		postReposiroty.saveAll(Arrays.asList(post1, post2));
 		
 //		maria.getPosts().addAll(Arrays.asList(post1, post2));
 //		userReposiroty.save(maria);
